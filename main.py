c27e7cd9b42dd322966ecef5ae6ee9b07b8287aa
Amorgane <83348086+Amorgane@users.noreply.github.com>
2022-11-27 19:05:52 -0500

    Création d'une grille de puissance 4 (pour l'instant dans le main, à mieux ranger)

--- main.py
+++ main.py
@@ -1,1 +1,42 @@
 
+from puissance4 import *
+
+from tkinter import *
+import tkinter as tk
+import numpy as np
+import time
+
+#Création de la fenêtre tkinter
+window = tk.Tk()
+window.geometry("700x600")
+# fond = tk.PhotoImage(file="fond_P4.png")
+# label1 = Label(window, image=fond)
+# label1.pack()
+
+hauteur = 600
+largeur = 700
+canvas = Canvas(window, width=largeur, height=hauteur, background='blue')
+for i in range (6) : #nombre de lignes
+    canvas.create_line(100*(i+1), 0, 100*(i+1), hauteur, width=5)
+for i in range (7) : #nombre de colonnes
+    canvas.create_line(0, 100*(i+1), largeur, 100*(i+1), width=5)
+canvas.pack()
+
+
+
+P4 = Puissance4()
+P4.print_board()
+
+
+
+
+#nécessaire pour laisser la fenêtre ouverte
+window.mainloop()
+
+
+
+
+
+
+
+
--- puissance4.py
+++ puissance4.py
@@ -1,54 +1,53 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Feb 23 18:30:17 2016
-@author: Maxime
+Created on :
+@author:
 """
 
-import numpy as np
-import random as rd
 from tkinter import *
-import P4_IA as IA
+import numpy as np
+import time
 
-
-def convert_coordinates(i, j):  # Converti les coordonnées dans le tableau en coordonnée sur la fenêtre
-    return Puissance4.H - (
-                Puissance4.hauteur - i) * Puissance4.H / Puissance4.hauteur, j * Puissance4.L / Puissance4.longueur
 
 
 class Puissance4(Tk):
-    column = 7.
-    line = 6.
-    width = 300
-    height = (line/column) * W
-    player = 1
+
 
 
     def callback(self, event):
-        print "Clic"
+        print ("Clic")
         self.frame.focus_set()
         x = event.x
-        x = int(x * Puissance4.width / Puissance4.W)
+        x = int(x * self.height / self.width)
         self.play(x, 1, True)
         ARTIF = IA.P4IA(self, 2)
         ARTIF.coup_IA(8)
 
     def __init__(self):
         Tk.__init__(self)
-        self.board = np.zeros(Puissance4.line, Puissance4.column)
-        self.frame = Frame(self, width=Puissance4.width, height=Puissance4.height)
-        self.canvas = Canvas(self, width=Puissance4.width, height=Puissance4.height)
+        self.column = 7
+        self.line = 6
+        self.width = 300
+        self.height = (float(self.line) / float(self.column)) * self.width
+        self.player = 1
+        self.board = np.zeros((self.line, self.column))
+        self.frame = Frame(self, width=self.width, height=self.height)
+        self.canvas = Canvas(self, width=self.width, height=self.height)
+        #Permet de resize les éléments quand on change la taille de la fenêtre
+        self.frame.pack()
+        self.canvas.pack()
 
     def reset(self):
-        self.board = np.zeros(Puissance4.line, Puissance4.column)
-        self.frame = Frame(self, width=Puissance4.width, height=Puissance4.height)
-        self.canvas = Canvas(self, width=Puissance4.width, height=Puissance4.height)
+        self.board = np.zeros(self.line, self.column)
+        self.frame = Frame(self, width=self.width, height=self.height)
+        self.canvas = Canvas(self, width=self.width, height=self.height)
 
     def possibles_plays(self):
         plays = []
-        for i in range(Puissance4.column):
-            for j in range(Puissance4.line):
-                if self.board[Puissance4.line-j-1,i] == 0:
-                    plays.append([Puissance4.line-j-1,i])
+        for i in range(self.column):
+            for j in range(self.line):
+                if self.board[self.line-j-1,i] == 0:
+                    plays.append([self.line-j-1,i])
                     break
 
         return plays
@@ -79,92 +78,111 @@ class Puissance4(Tk):
         return False
 
         # On regarde si le joueur gagne en colonne
-        if Puissance4.line-x >= 4:
+        if self.line-x >= 4:
             if self.board[x+1,y] == player & self.board[x+2,y] == player & self.board[x+3,y] == player:
                 print("player "+player+" win")
                 return True
 
-        if Puissance4.line-x <= 3:
+        if self.line-x <= 3:
             if self.board[x-1,y] == player & self.board[x-2,y] == player & self.board[x-3,y] == player:
                 print("player "+player+" win")
                 return True
 
-        if Puissance4.line - x >= 3 & x != 0:
+        if self.line - x >= 3 & x != 0:
             if self.board[x+1,y] == player & self.board[x+2,y] == player & self.board[x-1,y] == player:
                 print("player " + player + " win")
                 return True
 
-        if Puissance4.line - x <= 4 & x != Puissance4.line-1:
+        if self.line - x <= 4 & x != self.line-1:
             if self.board[x-1,y] == player & self.board[x-2,y] == player & self.board[x+1,y] == player:
                 print("player " + player + " win")
                 return True
 
         #On regarde si le joueur gagne en ligne
-        if Puissance4.column-y >= 4:
+        if self.column-y >= 4:
             if self.board[x,y+1] == player & self.board[x,y+2] == player & self.board[x,y+3] == player:
                 print("player "+player+" win")
                 return True
 
-        if Puissance4.column-y <= 4:
+        if self.column-y <= 4:
             if self.board[x,y-1] == player & self.board[x,y-2] == player & self.board[x,y-3] == player:
                 print("player "+player+" win")
                 return True
 
-        if Puissance4.column-y >= 3 & y != 0:
+        if self.column-y >= 3 & y != 0:
             if self.board[x,y+1] == player & self.board[x,y+2] == player & self.board[x,y-1] == player:
                 print("player "+player+" win")
                 return True
 
-        if Puissance4.column-y <= 5 & y != Puissance4.column-1:
+        if self.column-y <= 5 & y != self.column-1:
             if self.board[x,y-1] == player & self.board[x,y-2] == player & self.board[x,y+1] == player:
                 print("player "+player+" win")
                 return True
 
         #On regarde si le joueur gagne en diagonale
-        if Puissance4.line-x >= 4 & Puissance4.column-y >= 4:
+        if self.line-x >= 4 & self.column-y >= 4:
             if self.board[x+1,y+1] == player & self.board[x+2,y+2] == player & self.board[x+3,y+3] == player:
                 print("player "+player+" win")
                 return True
 
-        if Puissance4.line-x >= 3 & Puissance4.column-y >= 3 & x != 0 & y != 0:
+        if self.line-x >= 3 & self.column-y >= 3 & x != 0 & y != 0:
             if self.board[x+1, y+1] == player & self.board[x+2, y+2] == player & self.board[x-1,y-1] == player:
                 print("player "+player+" win")
                 return True
 
-        if Puissance4.line-x >= 4 & Puissance4.column-y <= 4:
+        if self.line-x >= 4 & self.column-y <= 4:
             if self.board[x+1,y-1] == player & self.board[x+2,y-2] == player & self.board[x+3,y-3] == player:
                 print("player " + player + " win")
                 return True
 
-        if Puissance4.line-x >= 3 & Puissance4.column-y <= 5 & x != 0 & y != Puissance4.column-1:
+        if self.line-x >= 3 & self.column-y <= 5 & x != 0 & y != self.column-1:
             if self.board[x+1,y-1] == player & self.board[x+2,y-2] == player & self.board[x-1,y+1] == player:
                 print("player " + player + " win")
                 return True
 
         # On regarde si le joueur gagne en diagonale
-        if Puissance4.line-x <= 3 & Puissance4.column-y >= 4:
+        if self.line-x <= 3 & self.column-y >= 4:
             if self.board[x-1,y+1] == player & self.board[x-2,y+2] == player & self.board[x-3,y+3] == player:
                 print("player " + player + " win")
                 return True
 
-        if Puissance4.line-x <= 4 & Puissance4.column-y >= 3 & x != Puissance4.line-1 & y != 0:
+        if self.line-x <= 4 & self.column-y >= 3 & x != self.line-1 & y != 0:
             if self.board[x-1,y+1] == player & self.board[x-2,y+2] == player & self.board[x+1,y-1] == player:
                 print("player " + player + " win")
                 return True
 
-        if Puissance4.line-x <= 3 & Puissance4.column-y <= 4:
+        if self.line-x <= 3 & self.column-y <= 4:
             if self.board[x-1,y-1] == player & self.board[x-2,y-2] == player & self.board[x-3,y-3] == player:
                 print("player " + player + " win")
                 return True
 
-        if Puissance4.line-x <= 4 & Puissance4.column-y <= 5 & x != Puissance4.line - 1 & y != Puissance4.column - 1:
+        if self.line-x <= 4 & self.column-y <= 5 & x != self.line - 1 & y != self.column - 1:
             if self.board[x-1,y-1] == player & self.board[x-2,y-2] == player & self.board[x+1,y+1] == player:
                 print("player " + player + " win")
                 return True
 
         return False
 
+    def convert_coordinates(self, i, j):  # Converti les coordonnées dans le tableau en coordonnée sur la fenêtre
+        return self.height - (
+                self.line - i) * self.height / self.column, j * self.L / self.width
+
     def print_board(self):
+
+        for i in range (self.line) :
+            for j in range (self.column) :
+                if (self.board[i,j]==1) :
+                    x0, y0 = self.convert_coordinates(i, j + 1)
+                    x1, y1 = self.convert_coordinates(i + 1, j)
+                    self.canvas.create_oval(y0, self.height - x0, y1, self.height - x1, outline="gray", fill="red",
+                                            width=2, tag="J1")
+                elif (self.board[i,j]==2) :
+                    x0, y0 = self.convert_coordinates(i, j + 1)
+                    x1, y1 = self.convert_coordinates(i + 1, j)
+                    self.canvas.create_oval(y0, self.height - x0, y1, self.height - x1, outline="gray", fill="yellow",
+                                            width=2, tag="J2")
+        self.frame.update()
+
 
 
 
